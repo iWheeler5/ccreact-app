6b6f8e9a0c7d30e4e317439146eacc7f308277b1
James Wheeler <wheelerjames139@hotmail.com>
2021-05-19 22:59:02 +0100

    added new code for adding reviews

--- backend/api/clubculture.route.js
+++ backend/api/clubculture.route.js
@@ -1,8 +1,16 @@
 import express from "express"
 import ClubsCtrl from "./clubs.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
 router.route("/clubs").get(ClubsCtrl.apiGetClubs)
+
+router
+    .route("/clubs/review")
+    .post(ReviewsCtrl.apiPostReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
+
 
 export default router
