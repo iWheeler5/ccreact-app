8d3a46d0f11e71e858132dfb20a982deb7c62c2e
James Wheeler <wheelerjames139@hotmail.com>
2021-09-07 17:11:20 +0100

    Updated lots and re-downloaded packages

--- frontend/src/components/pages/Clubs.js
+++ frontend/src/components/pages/Clubs.js
@@ -1,14 +1,69 @@
-import React from 'react'
-import '../../App.css';
+import React, { useState, useEffect } from "react";
+import ClubCultureDataService from "../../services/clubculture";
+import { Link } from "react-router-dom";
+import { Button } from "../Button";
 
-function Club() {
-    return(
-        <>
-        
+const Club = (props) => {
+  const initialClubState = {
+    id: null,
+    name: "",
+    description: "",
+    cityId: "",
+    capacity: "",
+    coverimgurl: "",
+    address: {},
+    reviews: [],
+  };
 
+  const [club, setClub] = useState(initialClubState);
 
-        </>
-    );
-}
+  const getClub = (id) => {
+    ClubCultureDataService.get(id)
+      .then((response) => {
+        setClub(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log(id);
+      });
+  };
+
+  useEffect(() => {
+    getClub(props.match.params.id);
+  }, [props.match.params.id]);
+
+  return (
+    <div className="club-page-container">
+      {club ? (
+        <div className="club-page-info">
+          <img
+            className="club-img"
+            src={club.coverimgurl}
+            alt="clubImage"
+          ></img>
+          <h5>{club.name}</h5>
+          <p>
+            <strong>Description: </strong>
+            {club.description}
+            <br />
+            <strong>Capacity: </strong>
+            {club.capacity}
+            <br />
+            <strong>Address: </strong>
+            {club.address.addressline1} {club.address.city}{" "}
+            {club.address.postcode}
+            <br />
+          </p>
+        </div>
+      ) : (
+        <div>
+          <br />
+          <p>No restaurant selected.</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Club;
--- frontend/src/components/pages/ClubsList.js
+++ frontend/src/components/pages/ClubsList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ClubCultureDataService from "../../services/clubculture";
 import { Link } from "react-router-dom";
+import { Button } from "../Button";
 //import '../../App.css';
 
 const ClubsList = (props) => {
@@ -49,6 +50,9 @@ const ClubsList = (props) => {
   };
 
   const refreshList = () => {
+    setSearchName("");
+    setSearchPostcode("");
+    setSearchCity("");
     retrieveClubs();
   };
 
@@ -72,14 +76,120 @@ const ClubsList = (props) => {
   };
 
   const findByCity = () => {
-    if (searchCity == "All Cities") {
+    if (searchCity === "All Cities") {
       refreshList();
     } else {
       find(searchCity, "city");
     }
   };
 
-  return <div className="hero-container"></div>;
+  return (
+    <div className="clubs-list">
+      <div className="row pb-1">
+        <div className="input-group col-lg-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by name"
+            value={searchName}
+            onChange={onChangeSearchName}
+          />
+          <div className="input-group-append">
+            {/* <Button className='btns' buttonStyle='btn--outline' 
+                buttonSize='btn--small' onClick={findByName} type="button">Search
+            </Button> */}
+            <button
+              className="btn btn--outline btn--search"
+              type="button"
+              onClick={findByName}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="input-group col-lg-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by Postcode"
+            value={searchPostcode}
+            onChange={onChangeSearchPostcode}
+          />
+          <div className="input-group-append">
+            {/* <Button className='btns' buttonStyle='btn--outline' 
+                buttonSize='btn--small' onClick={findByName} type="button">Search
+            </Button> */}
+            <button
+              className="btn btn--outline btn--search"
+              type="button"
+              onClick={findByPostcode}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="input-group col-lg-4">
+          <select onChange={onChangeSearchCity}>
+            {cities.map((city) => {
+              return <option value={city}> {city.substr(0, 20)}</option>;
+            })}
+          </select>
+          <div className="input-group-append">
+            <button
+              className="btn btn--outline btn--search"
+              type="button"
+              onClick={findByCity}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row pb-1">
+        <div className="input-group col-lg-4">
+          <button
+            className="btn btn--outline btn--large btn--clearresults"
+            type="button"
+            onClick={refreshList}
+          >
+            Clear Search Results
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        {clubs.map((club) => {
+          const address = `${club.address.addressline1} ${club.address.city} ${club.address.postcode}`;
+          const city = `${club.address.city}`;
+          return (
+            <div className="col-lg-7 pb-1 club-list-container">
+              <div className="card club-list-card">
+                <div className="card-body">
+                  <h5 className="card-title club-card-title">{club.name}</h5>
+                  {/* <p className="card-text">
+                    <strong>{club.city}</strong>
+                  </p> */}
+                  <p className="card-text">
+                    {city}
+                    <br />
+                    <br />
+                    <div className="row">
+                      <br />
+                      <Link
+                        to={"/clubs/" + club._id}
+                        className="btn btn--primary btn-club-fromlist"
+                      >
+                        View Club
+                      </Link>
+                    </div>
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ClubsList;
